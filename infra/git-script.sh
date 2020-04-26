fe1be0cb3ada38ec5f46c3294ab8db583f27bbf9
calee_cs <calee@cs.nctu.edu.tw>
2020-04-26 17:51:34 +0800

    Add commit formate template
    Because this is not a feature I want to write inside CHANGELOG, we don't
    need to and cannot follow the fomate of Conventional Commits or it will
    be save into CHANGELOG

--- infra/git-script.sh
+++ infra/git-script.sh
@@ -3,6 +3,7 @@
 # CRLF
 # config in local (only this project)
 git config core.autocrlf input
+git config commit.template ./infra/gitmessage
 
 # pre-commit hook
 cp ./pre-commit ../.git/hooks/pre-commit
