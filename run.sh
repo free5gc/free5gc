50f2240bf2d29588727f90e94f70b48f009ea900
s5uishida <s5u.ishida@gmail.com>
2020-08-25 13:07:13 +0900

    Revert "Stored the sudo process ID of n3iwf and its child process ID in PID_LIST."
    This reverts commit 28511a2c0f6e6fc3461dec0f1ab86e8217d5ce92.

--- run.sh
+++ run.sh
@@ -19,11 +19,8 @@ for NF in ${NF_LIST}; do
     PID_LIST+=($!)
 done
 
-sudo ./bin/n3iwf &
-SUDO_N3IWF_PID=$!
-sleep 1
-N3IWF_PID=`pgrep -P $SUDO_N3IWF_PID`
-PID_LIST+=($SUDO_N3IWF_PID $N3IWF_PID)
+./bin/n3iwf &
+PID_LIST+=($!)
 
 function terminate()
 {
