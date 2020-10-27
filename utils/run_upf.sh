ceeba7ac4d72d33454cc2aa09db90a345ea8c57a
João Paulo Lobianco Silva <joaopaulolobianco1@gmail.com>
2020-10-27 14:02:19 -0300

    Adjust config to release 3.0.4

--- utils/run_upf.sh
+++ utils/run_upf.sh
@@ -7,6 +7,6 @@ sudo ip netns exec UPFns ./bin/free5gc-upfd &
 PID_LIST+=($!)
 
 sleep 2
-sudo ip netns exec UPFns ip link set dev upfgtp0 mtu 1500
+sudo ip netns exec UPFns ip link set dev upfgtp mtu 1500
 
 wait ${PID_LIST}
