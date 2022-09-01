b38e0d1510b46e7ff25a5bffc01b03f6d3b5723a
ianchen0119 <ychen.desl@gmail.com>
2022-09-01 09:08:57 +0000

    add basic test in dispatch workflow

--- patch.sh
+++ patch.sh
@@ -2,11 +2,12 @@ NF_LIST="nrf amf smf udr pcf udm nssf ausf upf n3iwf"
 
 for NF in ${NF_LIST}; do
     cd NFs/$NF
+    git checkout main
     PATCH=("${NF^^}_PATCH")
     if [ "${!PATCH}" != "" ];
     then
-        echo "true"
         curl ${!PATCH} | git apply
+        git status
     fi
     cd ../..
 done
