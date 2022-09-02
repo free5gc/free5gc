f2e315b6addd9bfd119d1baa3d39d1bfb95e04a7
Ian Chen <ychen.desl@gmail.com>
2022-09-02 13:38:18 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,11 +1,15 @@
 NF_LIST="nrf amf smf udr pcf udm nssf ausf upf n3iwf"
 
+git checkout .
+
 for NF in ${NF_LIST}; do
     cd NFs/$NF
-    git checkout main
+    git checkout .
     PATCH=("${NF^^}_PATCH")
     if [ "${!PATCH}" != "" ];
     then
+        echo $NF
+        git checkout main
         curl ${!PATCH} | git apply
         git status
     fi
