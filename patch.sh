f8c5443688651976cfaedfc5b36ae857da5411d5
Allen00991 <allen00991.cs10@nycu.edu.tw>
2022-09-02 06:27:51 +0000

    remove uncommitted changes after dispatch event

--- patch.sh
+++ patch.sh
@@ -1,16 +1,14 @@
 NF_LIST="nrf amf smf udr pcf udm nssf ausf upf n3iwf"
 
-git checkout .
-
 for NF in ${NF_LIST}; do
     cd NFs/$NF
-    git checkout .
+    git checkout . # remove uncommited changes
+    git checkout main # switch to main branch
     PATCH=("${NF^^}_PATCH")
     if [ "${!PATCH}" != "" ];
     then
-        echo $NF
-        git checkout main
-        curl ${!PATCH} | git apply
+        echo $NF # print NF name
+        curl ${!PATCH} | git apply # apply patch
         git status
     fi
     cd ../..
