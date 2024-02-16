7cd16aeda16058f84c37e1c300f2716a1c3e84b4
Leo <oliveiraleo@users.noreply.github.com>
2024-02-16 06:17:41 -0300

    Update config reload script with better on screen messages (#528)

--- reload_host_config.sh
+++ reload_host_config.sh
@@ -3,7 +3,11 @@
 if [ -z "$1" ]
 then
     echo "[ERRO] No parameter was given!"
-    echo "[INFO] You must provide dn interface name as a parameter"
+    echo "[INFO] You must provide dn interface name as input parameter"
+    echo "Usage:"
+    echo "$0 <dn_interface>"
+    echo "Example:"
+    echo "$0 enp0s4"
 else
     echo "[INFO] Using $1 as interface name"
 
@@ -14,7 +18,7 @@ else
     echo -n "[INFO] Setting kernel net.ipv4.ip_forward flag... "
     sudo sysctl -w net.ipv4.ip_forward=1 >/dev/null
     echo "[OK]"
-    echo "[INFO] Disabling ufw firewall... "
+    echo -n "[INFO] Stopping ufw firewall... "
     sudo systemctl stop ufw
     echo "[OK]"
 
