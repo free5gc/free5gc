0b198dfec972b2f24b6ff3eaa419cae1fef64737
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2024-04-18 05:58:45 +0000

    feat: adjust force_kill.sh to drop all free5gc optionally

--- force_kill.sh
+++ force_kill.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+DROP_ALL_DB=0
+DB_NAME="free5gc"
+DB_DROP_COLLECTION=(
+    "NfProfile"
+    "applicationData.influenceData.subsToNotify"
+    "applicationData.subsToNotify"
+    "policyData.subsToNotify"
+    "exposureData.subsToNotify"
+)
+
+if [ $# -ne 0 ]; then
+    while [ $# -gt 0 ]; do
+        case $1 in
+            -db)
+                DROP_ALL_DB=1
+                ;;
+        esac
+        shift
+    done
+fi
+
 NF_LIST="nrf amf smf udr pcf udm nssf ausf n3iwf upf chf"
 
 for NF in ${NF_LIST}; do
@@ -19,5 +40,16 @@ sudo rm -f /tmp/free5gc_unix_sock
 sudo rm -f cert/*_*
 sudo rm -f test/cert/*_*
 sudo rm -f /tmp/config.json # CHF ChargingGatway FTP config
-mongo --eval "db.NfProfile.drop();db.applicationData.influenceData.subsToNotify.drop();db.applicationData.subsToNotify.drop();db.policyData.subsToNotify.drop();db.exposureData.subsToNotify.drop()" free5gc
-mongosh --eval "db.NfProfile.drop();db.applicationData.influenceData.subsToNotify.drop();db.applicationData.subsToNotify.drop();db.policyData.subsToNotify.drop();db.exposureData.subsToNotify.drop()" free5gc
+
+if [ $DROP_ALL_DB -eq 1 ]; then
+    mongo --eval "db.dropDatabase()" "$DB_NAME"
+    mongosh --eval "db.dropDatabase()" "$DB_NAME"
+else
+    MONGO_SCRIPT=""
+    for COLLECTION in "${DB_DROP_COLLECTION[@]}"
+    do
+        MONGO_SCRIPT+="db.$COLLECTION.drop();"
+    done
+    mongo "$DB_NAME" --eval "$MONGO_SCRIPT"
+    mongosh "$DB_NAME" --eval "$MONGO_SCRIPT" 
+fi
