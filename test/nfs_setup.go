b66a6f47f73b6d1fe67354cdd1290340d5adedbe
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2024-06-13 03:21:59 +0000

    refactor: add CreateNFs() in test and simply test init

--- test/nfs_setup.go
+++ test/nfs_setup.go
@@ -1,248 +1,242 @@
-package test_test
+package test
 
 import (
+	"context"
 	"fmt"
-	"os"
-	"os/signal"
-	"strings"
-	"syscall"
-	"testing"
+	"test/app"
 	"time"
 
-	"test"
-	"test/app"
-	"test/consumerTestdata/UDM/TestGenAuthData"
-
-	"github.com/stretchr/testify/assert"
+	"github.com/free5gc/openapi/models"
+	"github.com/free5gc/udm/pkg/suci"
 
 	amf_factory "github.com/free5gc/amf/pkg/factory"
+	amf_service "github.com/free5gc/amf/pkg/service"
 	ausf_factory "github.com/free5gc/ausf/pkg/factory"
-	"github.com/free5gc/nas/security"
-	"github.com/free5gc/ngap"
+	ausf_service "github.com/free5gc/ausf/pkg/service"
+	chf_factory "github.com/free5gc/chf/pkg/factory"
+	chf_service "github.com/free5gc/chf/pkg/service"
 	nrf_factory "github.com/free5gc/nrf/pkg/factory"
+	nrf_service "github.com/free5gc/nrf/pkg/service"
 	nssf_factory "github.com/free5gc/nssf/pkg/factory"
-	"github.com/free5gc/openapi/models"
+	nssf_service "github.com/free5gc/nssf/pkg/service"
 	pcf_factory "github.com/free5gc/pcf/pkg/factory"
+	pcf_service "github.com/free5gc/pcf/pkg/service"
 	smf_factory "github.com/free5gc/smf/pkg/factory"
+	smf_service "github.com/free5gc/smf/pkg/service"
 	udm_factory "github.com/free5gc/udm/pkg/factory"
-	"github.com/free5gc/udm/pkg/suci"
+	udm_service "github.com/free5gc/udm/pkg/service"
 	udr_factory "github.com/free5gc/udr/pkg/factory"
-	"github.com/free5gc/util/mongoapi"
+	udr_service "github.com/free5gc/udr/pkg/service"
 )
 
-// var NFs = []app.NetworkFunction{}
-
-var NFstructs = []app.NFstruct{}
+type TestId string
 
 const (
-	noInit = iota
-	initNF
-	multiAMF
+	TestCN                          TestId = "TestCN"
+	TestAFInfluenceOnTrafficRouting TestId = "TestAFInfluenceOnTrafficRouting"
+	TestRequestTwoPDUSessions       TestId = "TestRequestTwoPDUSessions"
 )
 
-var initFlag int = initNF
+type StartNFsConfig struct {
+	Nrf  bool `yaml:"nrf,omitempty" default:"false"`
+	Amf  bool `yaml:"amf,omitempty" default:"false"`
+	Smf  bool `yaml:"smf,omitempty" default:"false"`
+	Udr  bool `yaml:"udr,omitempty" default:"false"`
+	Pcf  bool `yaml:"pcf,omitempty" default:"false"`
+	Udm  bool `yaml:"udm,omitempty" default:"false"`
+	Nssf bool `yaml:"nssf,omitempty" default:"false"`
+	Ausf bool `yaml:"ausf,omitempty" default:"false"`
+	Chf  bool `yaml:"chf,omitempty" default:"false"`
 
-func init() {
-	initNfCfg := test.StartNFsConfig{
-		OAuth:  false,
-		TestId: "",
-	}
-
-	for _, arg := range os.Args {
-		values := strings.Split(arg, "=")
-		for k, v := range values {
-			if v == "-test.run" {
-				initNfCfg.TestId = test.TestId(values[k+1])
-				fmt.Printf("Run %s\n", initNfCfg.TestId)
-			}
-		}
-
-		if arg == "noinit" {
-			initFlag = noInit
-		}
-
-		if arg == "multiAmf" {
-			initFlag = multiAMF
-		}
-
-		if arg == "oauth" {
-			initNfCfg.OAuth = true
-		}
-	}
-
-	switch initFlag {
-	case multiAMF:
-		initLogPath()
-
-		startNfCfg := test.StartNFsConfig{
-			OAuth:  initNfCfg.OAuth,
-			TestId: initNfCfg.TestId,
-
-			Nrf:  true,
-			Smf:  true,
-			Udr:  true,
-			Pcf:  true,
-			Udm:  true,
-			Nssf: true,
-			Ausf: true,
-			Chf:  true,
-
-			Amf: false,
-		}
-
-		NFstructs = test.CreateNFs(startNfCfg)
-
-		NfStart()
-		setMongoDB()
-
-	case initNF:
-		initLogPath()
-
-		startNfCfg := test.StartNFsConfig{
-			OAuth:  initNfCfg.OAuth,
-			TestId: initNfCfg.TestId,
-
-			Nrf:  true,
-			Amf:  true,
-			Smf:  true,
-			Udr:  true,
-			Pcf:  true,
-			Udm:  true,
-			Nssf: true,
-			Ausf: true,
-			Chf:  true,
-		}
-		NFstructs = test.CreateNFs(startNfCfg)
-		NfStart()
-
-	default:
-		setMongoDB()
-	}
-
+	OAuth  bool   `yaml:"oauth,omitempty" default:"false"`
+	TestId TestId `yaml:"testId,omitempty"`
 }
 
-func initLogPath() {
-	// default key log path
-	if err := os.MkdirAll("./log/", 0775); err != nil {
-		fmt.Printf("Make directory %s failed: %+v", "./log/", err)
+func CreateNFs(cfg StartNFsConfig) []app.NFstruct {
+	var nfs []app.NFstruct
+
+	if cfg.Nrf {
+		nfs = append(nfs, NewNrfStruct(cfg.OAuth))
+	}
+	if cfg.Amf {
+		nfs = append(nfs, NewAmfStruct(cfg.TestId))
+	}
+	if cfg.Smf {
+		nfs = append(nfs, NewSmfStruct(cfg.TestId))
+	}
+	if cfg.Udr {
+		nfs = append(nfs, NewUdrStruct())
+	}
+	if cfg.Pcf {
+		nfs = append(nfs, NewPcfStruct())
+	}
+	if cfg.Udm {
+		nfs = append(nfs, NewUdmStruct())
+	}
+	if cfg.Nssf {
+		nfs = append(nfs, NewNssfStruct())
+	}
+	if cfg.Ausf {
+		nfs = append(nfs, NewAusfStruct())
+	}
+	if cfg.Chf {
+		nfs = append(nfs, NewChfStruct())
+	}
+	return nfs
+}
+
+func NewNrfStruct(oauth bool) app.NFstruct {
+	if err := nrfConfig(oauth); err != nil {
+		fmt.Printf("NRF Config failed: %v\n", err)
+	}
+	nrf_ctx, nrf_cancel := context.WithCancel(context.Background())
+	nrfApp, errApp := nrf_service.NewApp(nrf_factory.NrfConfig)
+	if errApp != nil {
+		fmt.Printf("NRF NewApp failed: %v\n", errApp)
+	}
+
+	return app.NFstruct{
+		Nf:     nrfApp,
+		Ctx:    &nrf_ctx,
+		Cancel: &nrf_cancel,
 	}
 }
 
-func setMongoDB() {
-	if err := mongoapi.SetMongoDB("free5gc", "mongodb://127.0.0.1:27017"); err != nil {
-		fmt.Printf("SetMongoDB failed: %v\n", err)
-		return
+func NewAmfStruct(testId TestId) app.NFstruct {
+	if err := amfConfig(testId); err != nil {
+		fmt.Printf("AMF Config failed: %v\n", err)
 	}
-	fmt.Println("MongoDB Set")
-}
-
-func NfStart() {
-	fmt.Println("NfStart", len(NFstructs))
-
-	for _, app := range NFstructs {
-		go app.Nf.Start("")
-		time.Sleep(200 * time.Millisecond)
+	amf_ctx, amf_cancel := context.WithCancel(context.Background())
+	amfApp, errApp := amf_service.NewApp(amf_factory.AmfConfig)
+	if errApp != nil {
+		fmt.Printf("AMF NewApp failed: %v\n", errApp)
 	}
-}
 
-func NfTerminate() {
-	if initFlag != noInit {
-		nfNums := len(NFstructs)
-		for i := nfNums - 1; i >= 0; i-- {
-			NFstructs[i].Nf.Terminate()
-		}
+	return app.NFstruct{
+		Nf:     amfApp,
+		Ctx:    &amf_ctx,
+		Cancel: &amf_cancel,
 	}
 }
 
-func TestNGSetup(t *testing.T) {
-	var n int
-	var sendMsg []byte
-	var recvMsg = make([]byte, 2048)
+func NewSmfStruct(testId TestId) app.NFstruct {
+	if err := smfConfig(testId); err != nil {
+		fmt.Printf("SMF Config failed: %v\n", err)
+	}
+	smf_ctx, smf_cancel := context.WithCancel(context.Background())
+	smfApp, errApp := smf_service.NewApp(smf_factory.SmfConfig)
+	if errApp != nil {
+		fmt.Printf("SMF NewApp failed: %v\n", errApp)
+	}
 
-	// RAN connect to AMF
-	conn, err := test.ConnectToAmf("127.0.0.1", "127.0.0.1", 38412, 9487)
-	assert.Nil(t, err)
-
-	// send NGSetupRequest Msg
-	sendMsg, err = test.GetNGSetupRequest([]byte("\x00\x01\x02"), 24, "free5gc")
-	assert.Nil(t, err)
-	_, err = conn.Write(sendMsg)
-	assert.Nil(t, err)
-
-	// receive NGSetupResponse Msg
-	n, err = conn.Read(recvMsg)
-	assert.Nil(t, err)
-	_, err = ngap.Decoder(recvMsg[:n])
-	assert.Nil(t, err)
-
-	// close Connection
-	conn.Close()
+	return app.NFstruct{
+		Nf:     smfApp,
+		Ctx:    &smf_ctx,
+		Cancel: &smf_cancel,
+	}
 }
 
-func TestCN(t *testing.T) {
-	// New UE
-	ue := test.NewRanUeContext("imsi-2089300007487", 1, security.AlgCiphering128NEA2, security.AlgIntegrity128NIA2,
-		models.AccessType__3_GPP_ACCESS)
-	// ue := test.NewRanUeContext("imsi-2089300007487", 1, security.AlgCiphering128NEA0, security.AlgIntegrity128NIA0, models.AccessType__3_GPP_ACCESS)
-	ue.AmfUeNgapId = 1
-	ue.AuthenticationSubs = test.GetAuthSubscription(TestGenAuthData.MilenageTestSet19.K,
-		TestGenAuthData.MilenageTestSet19.OPC,
-		TestGenAuthData.MilenageTestSet19.OP)
-	// insert UE data to MongoDB
-
-	servingPlmnId := "20893"
-	// test.InsertAuthSubscriptionToMongoDB(ue.Supi, ue.AuthenticationSubs)
-
-	test.InsertUeToMongoDB(t, ue, servingPlmnId)
-
-	getData := test.GetAuthSubscriptionFromMongoDB(ue.Supi)
-	assert.NotNil(t, getData)
-	{
-		amData := test.GetAccessAndMobilitySubscriptionData()
-		test.InsertAccessAndMobilitySubscriptionDataToMongoDB(ue.Supi, amData, servingPlmnId)
-		getData := test.GetAccessAndMobilitySubscriptionDataFromMongoDB(ue.Supi, servingPlmnId)
-		assert.NotNil(t, getData)
+func NewUdmStruct() app.NFstruct {
+	if err := udmConfig(); err != nil {
+		fmt.Printf("UDM Config failed: %v\n", err)
 	}
-	{
-		smfSelData := test.GetSmfSelectionSubscriptionData()
-		test.InsertSmfSelectionSubscriptionDataToMongoDB(ue.Supi, smfSelData, servingPlmnId)
-		getData := test.GetSmfSelectionSubscriptionDataFromMongoDB(ue.Supi, servingPlmnId)
-		assert.NotNil(t, getData)
-	}
-	{
-		smSelData := test.GetSessionManagementSubscriptionData()
-		test.InsertSessionManagementSubscriptionDataToMongoDB(ue.Supi, servingPlmnId, smSelData)
-		getData := test.GetSessionManagementDataFromMongoDB(ue.Supi, servingPlmnId)
-		assert.NotNil(t, getData)
-	}
-	{
-		amPolicyData := test.GetAmPolicyData()
-		test.InsertAmPolicyDataToMongoDB(ue.Supi, amPolicyData)
-		getData := test.GetAmPolicyDataFromMongoDB(ue.Supi)
-		assert.NotNil(t, getData)
-	}
-	{
-		smPolicyData := test.GetSmPolicyData()
-		test.InsertSmPolicyDataToMongoDB(ue.Supi, smPolicyData)
-		getData := test.GetSmPolicyDataFromMongoDB(ue.Supi)
-		assert.NotNil(t, getData)
+	udm_ctx, udm_cancel := context.WithCancel(context.Background())
+	udmApp, errApp := udm_service.NewApp(udm_factory.UdmConfig)
+	if errApp != nil {
+		fmt.Printf("UDM NewApp failed: %v\n", errApp)
 	}
 
-	defer beforeClose(t, ue, servingPlmnId)
-
-	// subscribe os signal
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Signal(syscall.SIGUSR1))
-	<-c
+	return app.NFstruct{
+		Nf:     udmApp,
+		Ctx:    &udm_ctx,
+		Cancel: &udm_cancel,
+	}
 }
 
-func beforeClose(t *testing.T, ue *test.RanUeContext, servingPlmnId string) {
-	// delete test data
-	// test.DelAuthSubscriptionToMongoDB(ue.Supi)
-	// test.DelAccessAndMobilitySubscriptionDataFromMongoDB(ue.Supi, "20893")
-	// test.DelSmfSelectionSubscriptionDataFromMongoDB(ue.Supi, "20893")
+func NewPcfStruct() app.NFstruct {
+	if err := pcfConfig(); err != nil {
+		fmt.Printf("PCF Config failed: %v\n", err)
+	}
+	pcf_ctx, pcf_cancel := context.WithCancel(context.Background())
+	pcfApp, errApp := pcf_service.NewApp(pcf_factory.PcfConfig)
+	if errApp != nil {
+		fmt.Printf("PCF NewApp failed: %v\n", errApp)
+	}
 
-	test.DelUeFromMongoDB(t, ue, servingPlmnId)
+	return app.NFstruct{
+		Nf:     pcfApp,
+		Ctx:    &pcf_ctx,
+		Cancel: &pcf_cancel,
+	}
+}
+
+func NewUdrStruct() app.NFstruct {
+	if err := udrConfig(); err != nil {
+		fmt.Printf("UDR Config failed: %v\n", err)
+	}
+	udr_ctx, udr_cancel := context.WithCancel(context.Background())
+	udrApp, errApp := udr_service.NewApp(udr_factory.UdrConfig)
+	if errApp != nil {
+		fmt.Printf("UDR NewApp failed: %v\n", errApp)
+	}
+
+	return app.NFstruct{
+		Nf:     udrApp,
+		Ctx:    &udr_ctx,
+		Cancel: &udr_cancel,
+	}
+}
+
+func NewNssfStruct() app.NFstruct {
+	if err := nssfConfig(); err != nil {
+		fmt.Printf("NSSF Config failed: %v\n", err)
+	}
+	nssf_ctx, nssf_cancel := context.WithCancel(context.Background())
+	nssfApp, errApp := nssf_service.NewApp(nssf_factory.NssfConfig)
+	if errApp != nil {
+		fmt.Printf("NSSF NewApp failed: %v\n", errApp)
+	}
+
+	return app.NFstruct{
+		Nf:     nssfApp,
+		Ctx:    &nssf_ctx,
+		Cancel: &nssf_cancel,
+	}
+}
+
+func NewAusfStruct() app.NFstruct {
+	if err := ausfConfig(); err != nil {
+		fmt.Printf("AUSF Config failed: %v\n", err)
+	}
+
+	ausf_ctx, ausf_cancel := context.WithCancel(context.Background())
+	ausfApp, errApp := ausf_service.NewApp(ausf_factory.AusfConfig)
+	if errApp != nil {
+		fmt.Printf("AUSF NewApp failed: %v\n", errApp)
+	}
+
+	return app.NFstruct{
+		Nf:     ausfApp,
+		Ctx:    &ausf_ctx,
+		Cancel: &ausf_cancel,
+	}
+}
+
+func NewChfStruct() app.NFstruct {
+	if err := chfConfig(); err != nil {
+		fmt.Printf("CHF Config failed: %v\n", err)
+	}
+	chf_ctx, chf_cancel := context.WithCancel(context.Background())
+	chfApp, errApp := chf_service.NewApp(chf_ctx, chf_factory.ChfConfig, "")
+	if errApp != nil {
+		fmt.Printf("CHF NewApp failed: %v\n", errApp)
+	}
+
+	return app.NFstruct{
+		Nf:     chfApp,
+		Ctx:    &chf_ctx,
+		Cancel: &chf_cancel,
+	}
 }
 
 func nrfConfig(oauth bool) error {
@@ -288,14 +282,13 @@ func nrfConfig(oauth bool) error {
 	if _, err := nrf_factory.NrfConfig.Validate(); err != nil {
 		return err
 	}
-
 	return nil
 }
 
-func amfConfig(testID string) error {
+func amfConfig(testID TestId) error {
 	var ngapIpList, integrityOrder, cipheringOrder []string
 
-	if testID == "TestCN" {
+	if testID == TestCN {
 		ngapIpList = []string{"10.200.200.1"}
 		integrityOrder = []string{"NIA2", "NIA0"}
 		cipheringOrder = []string{"NEA2", "NEA0"}
@@ -443,10 +436,10 @@ func amfConfig(testID string) error {
 	return nil
 }
 
-func smfConfig(testID string) error {
+func smfConfig(testID TestId) error {
 	var dnaiList []string
 
-	if testID == "TestAFInfluenceOnTrafficRouting" {
+	if testID == TestAFInfluenceOnTrafficRouting {
 		dnaiList = []string{"edge"}
 	}
 
@@ -1373,5 +1366,77 @@ func ausfConfig() error {
 		return err
 	}
 
+	return nil
+}
+
+func chfConfig() error {
+	chf_factory.ChfConfig = &chf_factory.Config{
+		Info: &chf_factory.Info{
+			Version:     "1.0.3",
+			Description: "CHF initial test configuration",
+		},
+		Configuration: &chf_factory.Configuration{
+			ChfName: "CHF",
+			Sbi: &chf_factory.Sbi{
+				Scheme:       "http",
+				RegisterIPv4: "127.0.0.113",
+				BindingIPv4:  "127.0.0.113",
+				Port:         8000,
+				Tls: &chf_factory.Tls{
+					Pem: "../cert/chf.pem",
+					Key: "../cert/chf.key",
+				},
+			},
+			NrfUri:     "http://127.0.0.10:8000",
+			NrfCertPem: "../cert/nrf.pem",
+			ServiceNameList: []string{
+				"nchf-convergedcharging",
+			},
+			Mongodb: &chf_factory.Mongodb{
+				Name: "free5gc",
+				Url:  "mongodb://localhost:27017",
+			},
+			QuotaValidityTime:   10000,
+			VolumeLimit:         50000,
+			VolumeLimitPDU:      10000,
+			VolumeThresholdRate: 0.8,
+			Cgf: &chf_factory.Cgf{
+				HostIPv4:   "127.0.0.1",
+				Port:       2121,
+				ListenPort: 2122,
+				Tls: &chf_factory.Tls{
+					Pem: "../cert/chf.pem",
+					Key: "../cert/chf.key",
+				},
+			},
+			AbmfDiameter: &chf_factory.Diameter{
+				Protocol: "tcp",
+				HostIPv4: "127.0.0.113",
+				Port:     3868,
+				Tls: &chf_factory.Tls{
+					Pem: "../cert/chf.pem",
+					Key: "../cert/chf.key",
+				},
+			},
+			RfDiameter: &chf_factory.Diameter{
+				Protocol: "tcp",
+				HostIPv4: "127.0.0.113",
+				Port:     3869,
+				Tls: &chf_factory.Tls{
+					Pem: "../cert/chf.pem",
+					Key: "../cert/chf.key",
+				},
+			},
+		},
+		Logger: &chf_factory.Logger{
+			Enable:       true,
+			Level:        "info",
+			ReportCaller: false,
+		},
+	}
+
+	if _, err := chf_factory.ChfConfig.Validate(); err != nil {
+		return err
+	}
 	return nil
 }
