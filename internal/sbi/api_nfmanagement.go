87b6eae5ac91ad935f5f77a224b9aabb673b16df
kun <kun.cs12@nycu.edu.tw>
2024-10-01 13:34:01 +0000

    chore: delete some useless functions

--- internal/sbi/api_nfmanagement.go
+++ internal/sbi/api_nfmanagement.go
@@ -18,15 +18,15 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/mitchellh/mapstructure"
-	"go.mongodb.org/mongo-driver/bson"
+	// "go.mongodb.org/mongo-driver/bson"
 
-	nrf_context "github.com/free5gc/nrf/internal/context"
+	// nrf_context "github.com/free5gc/nrf/internal/context"
 	"github.com/free5gc/nrf/internal/logger"
 	"github.com/free5gc/nrf/internal/util"
 	"github.com/free5gc/openapi"
 	"github.com/free5gc/openapi/models"
-	timedecode "github.com/free5gc/util/mapstruct"
-	"github.com/free5gc/util/mongoapi"
+	// timedecode "github.com/free5gc/util/mapstruct"
+	// "github.com/free5gc/util/mongoapi"
 )
 
 func (s *Server) getNfRegisterRoute() []Route {
@@ -312,267 +312,275 @@ func (s *Server) HTTPCreateSubscription(c *gin.Context) {
 	s.Processor().HandleCreateSubscriptionRequest(c, subscription)
 }
 
-func (s *Server) GetNrfInfo() *models.NrfInfo {
-	// init
-	var nrfinfo models.NrfInfo
+// func ConvertInterface[T any](Origin map[string]T) map[string]interface{} {
+// 	result := make(map[string]interface{})
+// 	for key, value := range Origin {
+// 		result[key] = value
+// 	}
+// 	return result
+// }
 
-	//nrfinfo.ServedUdrInfo = s.getUdrInfo()
-	//nrfinfo.ServedUdmInfo = s.getUdmInfo()
-	//nrfinfo.ServedAusfInfo = s.getAusfInfo()
-	nrfinfo.ServedAmfInfo = s.getAmfInfo()
-	//nrfinfo.ServedSmfInfo = s.getSmfInfo()
-	//nrfinfo.ServedUpfInfo = s.getUpfInfo()
-	//nrfinfo.ServedPcfInfo = s.getPcfInfo()
-	nrfinfo.ServedBsfInfo = s.getBsfInfo()
-	//nrfinfo.ServedChfInfo = s.getChfInfo()
+// func (s *Server) GetNrfInfo() *models.NrfInfo {
+// 	 //init
+// 	var nrfinfo models.NrfInfo
 
-	return &nrfinfo
-}
+// 	nrfinfo.ServedUdrInfo = ConvertInterface(s.getUdrInfo())
+// 	nrfinfo.ServedUdmInfo = ConvertInterface(s.getUdmInfo())
+// 	nrfinfo.ServedAusfInfo = ConvertInterface(s.getAusfInfo())
+// 	nrfinfo.ServedAmfInfo = ConvertInterface(s.getAmfInfo())
+// 	nrfinfo.ServedSmfInfo = ConvertInterface(s.getSmfInfo())
+// 	nrfinfo.ServedUpfInfo = ConvertInterface(s.getUpfInfo())
+// 	nrfinfo.ServedPcfInfo = ConvertInterface(s.getPcfInfo())
+// 	nrfinfo.ServedBsfInfo = ConvertInterface(s.getBsfInfo())
+// 	nrfinfo.ServedChfInfo = ConvertInterface(s.getChfInfo())
 
-func (s *Server) getUdrInfo() map[string]interface{} {
-	servedUdrInfo := make(map[string]interface{})
-	var UDRProfile models.NrfNfManagementNfProfile
+// 	return &nrfinfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "UDR"}
+// func (s *Server) getUdrInfo() map[string]models.UdrInfo {
+// 	servedUdrInfo := make(map[string]models.UdrInfo)
+// 	var UDRProfile models.NrfNfManagementNfProfile
 
-	UDR, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getUdrInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "UDR"}
 
-	var UDRStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(UDR, &UDRStruct); err != nil {
-		logger.NfmLog.Errorf("getUdrInfo err: %+v", err)
-	}
+// 	UDR, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getUdrInfo err: %+v", err)
+// 	}
 
-	for i := 0; i < len(UDRStruct); i++ {
-		err = mapstructure.Decode(UDRStruct[i], &UDRProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedUdrInfo[index] = *UDRProfile.UdrInfo
-	}
-	return servedUdrInfo
-}
+// 	var UDRStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(UDR, &UDRStruct); err != nil {
+// 		logger.NfmLog.Errorf("getUdrInfo err: %+v", err)
+// 	}
 
-func (s *Server) getUdmInfo() map[string]models.UdmInfo {
-	servedUdmInfo := make(map[string]models.UdmInfo)
-	var UDMProfile models.NrfNfManagementNfProfile
+// 	for i := 0; i < len(UDRStruct); i++ {
+// 		err = mapstructure.Decode(UDRStruct[i], &UDRProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedUdrInfo[index] = *UDRProfile.UdrInfo
+// 	}
+// 	return servedUdrInfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "UDM"}
+// func (s *Server) getUdmInfo() map[string]models.UdmInfo {
+// 	servedUdmInfo := make(map[string]models.UdmInfo)
+// 	var UDMProfile models.NrfNfManagementNfProfile
 
-	UDM, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getUdmInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "UDM"}
 
-	var UDMStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(UDM, &UDMStruct); err != nil {
-		logger.NfmLog.Errorf("getUdmInfo err: %+v", err)
-	}
+// 	UDM, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getUdmInfo err: %+v", err)
+// 	}
 
-	for i := 0; i < len(UDMStruct); i++ {
-		err = mapstructure.Decode(UDMStruct[i], &UDMProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedUdmInfo[index] = *UDMProfile.UdmInfo
-	}
-	return servedUdmInfo
-}
+// 	var UDMStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(UDM, &UDMStruct); err != nil {
+// 		logger.NfmLog.Errorf("getUdmInfo err: %+v", err)
+// 	}
 
-func (s *Server) getAusfInfo() map[string]models.AusfInfo {
-	servedAusfInfo := make(map[string]models.AusfInfo)
-	var AUSFProfile models.NrfNfManagementNfProfile
+// 	for i := 0; i < len(UDMStruct); i++ {
+// 		err = mapstructure.Decode(UDMStruct[i], &UDMProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedUdmInfo[index] = *UDMProfile.UdmInfo
+// 	}
+// 	return servedUdmInfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "AUSF"}
+// func (s *Server) getAusfInfo() map[string]models.AusfInfo {
+// 	servedAusfInfo := make(map[string]models.AusfInfo)
+// 	var AUSFProfile models.NrfNfManagementNfProfile
 
-	AUSF, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getAusfInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "AUSF"}
 
-	var AUSFStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(AUSF, &AUSFStruct); err != nil {
-		logger.NfmLog.Errorf("getAusfInfo err: %+v", err)
-	}
-	for i := 0; i < len(AUSFStruct); i++ {
-		err = mapstructure.Decode(AUSFStruct[i], &AUSFProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedAusfInfo[index] = *AUSFProfile.AusfInfo
-	}
-	return servedAusfInfo
-}
+// 	AUSF, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getAusfInfo err: %+v", err)
+// 	}
 
-func (s *Server) getAmfInfo() map[string]models.AmfInfo {
-	servedAmfinfo := make(map[string]models.AmfInfo)
-	var AMFProfile models.NrfNfManagementNfProfile
+// 	var AUSFStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(AUSF, &AUSFStruct); err != nil {
+// 		logger.NfmLog.Errorf("getAusfInfo err: %+v", err)
+// 	}
+// 	for i := 0; i < len(AUSFStruct); i++ {
+// 		err = mapstructure.Decode(AUSFStruct[i], &AUSFProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedAusfInfo[index] = *AUSFProfile.AusfInfo
+// 	}
+// 	return servedAusfInfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "AMF"}
+// func (s *Server) getAmfInfo() map[string]models.NrfNfManagementAmfInfo {
+// 	servedAmfinfo := make(map[string]models.NrfNfManagementAmfInfo)
+// 	var AMFProfile models.NrfNfManagementNfProfile
 
-	AMF, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getAmfInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "AMF"}
 
-	var AMFStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(AMF, &AMFStruct); err != nil {
-		logger.NfmLog.Errorf("getAmfInfo err: %+v", err)
-	}
-	for i := 0; i < len(AMFStruct); i++ {
-		err = mapstructure.Decode(AMFStruct[i], &AMFProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedAmfinfo[index] = *AMFProfile.AmfInfo
-	}
-	return servedAmfinfo
-}
+// 	AMF, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getAmfInfo err: %+v", err)
+// 	}
 
-func (s *Server) getSmfInfo() map[string]models.SmfInfo {
-	servedSmfInfo := make(map[string]models.SmfInfo)
-	var SMFProfile models.NrfNfManagementNfProfile
+// 	var AMFStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(AMF, &AMFStruct); err != nil {
+// 		logger.NfmLog.Errorf("getAmfInfo err: %+v", err)
+// 	}
+// 	for i := 0; i < len(AMFStruct); i++ {
+// 		err = mapstructure.Decode(AMFStruct[i], &AMFProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedAmfinfo[index] = *AMFProfile.AmfInfo
+// 	}
+// 	return servedAmfinfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "SMF"}
+// func (s *Server) getSmfInfo() map[string]models.SmfInfo {
+// 	servedSmfInfo := make(map[string]models.SmfInfo)
+// 	var SMFProfile models.NrfNfManagementNfProfile
 
-	SMF, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getSmfInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "SMF"}
 
-	var SMFStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(SMF, &SMFStruct); err != nil {
-		logger.NfmLog.Errorf("getSmfInfo err: %+v", err)
-	}
-	for i := 0; i < len(SMFStruct); i++ {
-		err = mapstructure.Decode(SMFStruct[i], &SMFProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedSmfInfo[index] = *SMFProfile.SmfInfo
-	}
-	return servedSmfInfo
-}
+// 	SMF, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getSmfInfo err: %+v", err)
+// 	}
 
-func (s *Server) getUpfInfo() map[string]models.UpfInfo {
-	servedUpfInfo := make(map[string]models.UpfInfo)
-	var UPFProfile models.NrfNfManagementNfProfile
+// 	var SMFStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(SMF, &SMFStruct); err != nil {
+// 		logger.NfmLog.Errorf("getSmfInfo err: %+v", err)
+// 	}
+// 	for i := 0; i < len(SMFStruct); i++ {
+// 		err = mapstructure.Decode(SMFStruct[i], &SMFProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedSmfInfo[index] = *SMFProfile.SmfInfo
+// 	}
+// 	return servedSmfInfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "UPF"}
+// func (s *Server) getUpfInfo() map[string]models.UpfInfo {
+// 	servedUpfInfo := make(map[string]models.UpfInfo)
+// 	var UPFProfile models.NrfNfManagementNfProfile
 
-	UPF, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getUpfInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "UPF"}
 
-	var UPFStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(UPF, &UPFStruct); err != nil {
-		logger.NfmLog.Errorf("getUpfInfo err: %+v", err)
-	}
-	for i := 0; i < len(UPFStruct); i++ {
-		err = mapstructure.Decode(UPFStruct[i], &UPFProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedUpfInfo[index] = *UPFProfile.UpfInfo
-	}
-	return servedUpfInfo
-}
+// 	UPF, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getUpfInfo err: %+v", err)
+// 	}
 
-func (s *Server) getPcfInfo() map[string]models.PcfInfo {
-	servedPcfInfo := make(map[string]models.PcfInfo)
-	var PCFProfile models.NrfNfManagementNfProfile
+// 	var UPFStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(UPF, &UPFStruct); err != nil {
+// 		logger.NfmLog.Errorf("getUpfInfo err: %+v", err)
+// 	}
+// 	for i := 0; i < len(UPFStruct); i++ {
+// 		err = mapstructure.Decode(UPFStruct[i], &UPFProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedUpfInfo[index] = *UPFProfile.UpfInfo
+// 	}
+// 	return servedUpfInfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "PCF"}
+// func (s *Server) getPcfInfo() map[string]models.PcfInfo {
+// 	servedPcfInfo := make(map[string]models.PcfInfo)
+// 	var PCFProfile models.NrfNfManagementNfProfile
 
-	PCF, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getPcfInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "PCF"}
 
-	var PCFStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(PCF, &PCFStruct); err != nil {
-		logger.NfmLog.Errorf("getPcfInfo err: %+v", err)
-	}
-	for i := 0; i < len(PCFStruct); i++ {
-		err = mapstructure.Decode(PCFStruct[i], &PCFProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedPcfInfo[index] = *PCFProfile.PcfInfo
-	}
-	return servedPcfInfo
-}
+// 	PCF, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getPcfInfo err: %+v", err)
+// 	}
 
-func (s *Server) getBsfInfo() map[string]models.BsfInfo {
-	servedBsfInfo := make(map[string]models.BsfInfo)
-	var BSFProfile models.NrfNfManagementNfProfile
+// 	var PCFStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(PCF, &PCFStruct); err != nil {
+// 		logger.NfmLog.Errorf("getPcfInfo err: %+v", err)
+// 	}
+// 	for i := 0; i < len(PCFStruct); i++ {
+// 		err = mapstructure.Decode(PCFStruct[i], &PCFProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedPcfInfo[index] = *PCFProfile.PcfInfo
+// 	}
+// 	return servedPcfInfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "BSF"}
+// func (s *Server) getBsfInfo() map[string]models.NrfNfManagementBsfInfo {
+// 	servedBsfInfo := make(map[string]models.NrfNfManagementBsfInfo)
+// 	var BSFProfile models.NrfNfManagementNfProfile
 
-	BSF, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getBsfInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "BSF"}
 
-	var BSFStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(BSF, &BSFStruct); err != nil {
-		logger.NfmLog.Errorf("getBsfInfo err: %+v", err)
-	}
-	for i := 0; i < len(BSFStruct); i++ {
-		err = mapstructure.Decode(BSFStruct[i], &BSFProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedBsfInfo[index] = *BSFProfile.BsfInfo
-	}
-	return servedBsfInfo
-}
+// 	BSF, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getBsfInfo err: %+v", err)
+// 	}
 
-func (s *Server) getChfInfo() map[string]models.ChfInfo {
-	servedChfInfo := make(map[string]models.ChfInfo)
-	var CHFProfile models.NrfNfManagementNfProfile
+// 	var BSFStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(BSF, &BSFStruct); err != nil {
+// 		logger.NfmLog.Errorf("getBsfInfo err: %+v", err)
+// 	}
+// 	for i := 0; i < len(BSFStruct); i++ {
+// 		err = mapstructure.Decode(BSFStruct[i], &BSFProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedBsfInfo[index] = *BSFProfile.BsfInfo
+// 	}
+// 	return servedBsfInfo
+// }
 
-	collName := nrf_context.NfProfileCollName
-	filter := bson.M{"nfType": "CHF"}
+// func (s *Server) getChfInfo() map[string]models.ChfInfo {
+// 	servedChfInfo := make(map[string]models.ChfInfo)
+// 	var CHFProfile models.NrfNfManagementNfProfile
 
-	CHF, err := mongoapi.RestfulAPIGetMany(collName, filter)
-	if err != nil {
-		logger.NfmLog.Errorf("getChfInfo err: %+v", err)
-	}
+// 	collName := nrf_context.NfProfileCollName
+// 	filter := bson.M{"nfType": "CHF"}
 
-	var CHFStruct []models.NrfNfManagementNfProfile
-	if err = timedecode.Decode(CHF, &CHFStruct); err != nil {
-		logger.NfmLog.Errorf("getChfInfo err: %+v", err)
-	}
-	for i := 0; i < len(CHFStruct); i++ {
-		err = mapstructure.Decode(CHFStruct[i], &CHFProfile)
-		if err != nil {
-			panic(err)
-		}
-		index := strconv.Itoa(i)
-		servedChfInfo[index] = *CHFProfile.ChfInfo
-	}
-	return servedChfInfo
-}
+// 	CHF, err := mongoapi.RestfulAPIGetMany(collName, filter)
+// 	if err != nil {
+// 		logger.NfmLog.Errorf("getChfInfo err: %+v", err)
+// 	}
+
+// 	var CHFStruct []models.NrfNfManagementNfProfile
+// 	if err = timedecode.Decode(CHF, &CHFStruct); err != nil {
+// 		logger.NfmLog.Errorf("getChfInfo err: %+v", err)
+// 	}
+// 	for i := 0; i < len(CHFStruct); i++ {
+// 		err = mapstructure.Decode(CHFStruct[i], &CHFProfile)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		index := strconv.Itoa(i)
+// 		servedChfInfo[index] = *CHFProfile.ChfInfo
+// 	}
+// 	return servedChfInfo
+// }
 
 // DecodeNfProfile - Only support []map[string]interface to []models.NfProfile
 func (s *Server) DecodeNfProfile(source interface{}, format string) (models.NrfNfManagementNfProfile, error) {
