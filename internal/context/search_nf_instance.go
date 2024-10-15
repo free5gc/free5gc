1d57b227f6d160d7315c43ac732c964d1bfb2b14
kun <kun.cs12@nycu.edu.tw>
2024-10-15 15:12:25 +0000

    feat: add R17 new attributes for SearchNFInstances

--- internal/context/search_nf_instance.go
+++ internal/context/search_nf_instance.go
@@ -17,8 +17,11 @@ type SearchNFInstances struct {
 	Snssais                 []models.Snssai              `form:"snssais" `
 	PlmnSpecificSnssaiList  []models.PlmnSnssai          `form:"plmn-specific-snssai-list"`
 	Dnn                     string                       `form:"dnn" `
+	Ipv4Index               []string                     `form:"ipv4-index"`
+	Ipv6Index               []string                     `form:"ipv6-index"`
 	NsiList                 []string                     `form:"nsi-list" `
 	SmfServingArea          string                       `form:"smf-serving-area" `
+	MbSmfServingArea        string                       `form:"mbsmf-serving-area" `
 	Tai                     models.Tai                   `form:"tai" `
 	AmfRegionID             string                       `form:"amf-region-id" `
 	AmfSetID                string                       `form:"amf-set-id" `
@@ -29,12 +32,18 @@ type SearchNFInstances struct {
 	UeIpv6Prefix            string                       `form:"ue-ipv6-prefix" `
 	PgwInd                  bool                         `form:"pgw-ind" `
 	Pgw                     string                       `form:"pgw" `
+	PgwIP                   string                       `form:"pgw-ip" `
 	Gpsi                    string                       `form:"gpsi" `
 	ExternalGroupIdentity   string                       `form:"external-group-identity" `
+	InternalGroupIdentity   string                       `form:"internal-group-identity" `
+	PfdData                 string                       `form:"pfd-data" `
 	DataSet                 models.DataSetId             `form:"data-set" `
 	RoutingIndicator        string                       `form:"routing-indicator" `
 	GroupIDList             []string                     `form:"group-id-list" `
 	DnaiList                []string                     `form:"dnai-list" `
+	PduSessionType          []models.PduSessionType      `form:"pdu-session-typs" `
+	EventIdList             []string                     `form:"event-id-list" `
+	NwdafEventList          []models.NwdafEvent          `form:"nwdaf-event-list" `
 	SupportedFeatures       []string                     `form:"supported-features" `
 	UpfIwkEpsInd            bool                         `form:"upf-iwk-eps-ind" `
 	ChfSupportedPlmn        models.PlmnId                `form:"chf-supported-plmn" `
