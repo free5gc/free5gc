6784b7cb7a6936a627959cdef11b949baa064e55
CTFang@WireLab <ctfang.cs12@nycu.edu.tw>
2024-10-24 05:12:18 +0000

    fix: nf discovery filter & token request reflect type

--- internal/sbi/api_accesstoken.go
+++ internal/sbi/api_accesstoken.go
@@ -80,7 +80,8 @@ func (s *Server) HTTPAccessTokenRequest(c *gin.Context) {
 				break
 			}
 		}
-		if vt.Name() == "string" || vt.Name() == "NfType" {
+		if vt == reflect.TypeOf("") || vt == reflect.TypeOf(models.NrfNfManagementNfType_NRF) {
+			// Type is string
 			reflect.ValueOf(&accessTokenReq).Elem().FieldByName(name).SetString(value[0])
 		} else {
 			plmnid := models.PlmnId{}
