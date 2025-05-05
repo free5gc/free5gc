2268c69c0d6dc73757c4187bc7dfdd2819126aae
Alonza0314 <dufengplus@gmail.com>
2025-05-05 15:11:25 +0000

    feat: add ICMP conn write test

--- test/nrdc_test.go
+++ test/nrdc_test.go
@@ -1,6 +1,7 @@
 package test_test
 
 import (
+	"encoding/hex"
 	"net"
 	"test"
 	"test/consumerTestdata/UDM/TestGenAuthData"
@@ -21,6 +22,8 @@ import (
 	"github.com/free5gc/sctp"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"golang.org/x/net/icmp"
+	"golang.org/x/net/ipv4"
 )
 
 const (
@@ -388,6 +391,88 @@ func pduSessionEstablishment(t *testing.T, ue *test.RanUeContext, MranConn *sctp
 	time.Sleep(1 * time.Second)
 }
 
+func icmpTest(t *testing.T, MupfConn *net.UDPConn, SupfConn *net.UDPConn) {
+	// Send ICMP Echo Request through Master RAN
+	gtpHdr, err := hex.DecodeString("32ff00340000000100000000")
+	assert.Nil(t, err)
+	icmpData, err := hex.DecodeString("8c870d0000000000101112131415161718191a1b1c1d1e1f202122232425262728292a2b2c2d2e2f3031323334353637")
+	assert.Nil(t, err)
+
+	// Send through Master RAN
+	ipv4hdr := ipv4.Header{
+		Version:  4,
+		Len:      20,
+		Protocol: 1,
+		Flags:    0,
+		TotalLen: 48,
+		TTL:      64,
+		Src:      net.ParseIP("10.60.0.1").To4(),
+		Dst:      net.ParseIP("10.60.0.101").To4(),
+		ID:       1,
+	}
+	checksum := test.CalculateIpv4HeaderChecksum(&ipv4hdr)
+	ipv4hdr.Checksum = int(checksum)
+
+	v4HdrBuf, err := ipv4hdr.Marshal()
+	assert.Nil(t, err)
+	tt := append(gtpHdr, v4HdrBuf...)
+
+	m := icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: 12394, Seq: 1,
+			Data: icmpData,
+		},
+	}
+	b, err := m.Marshal(nil)
+	assert.Nil(t, err)
+	b[2] = 0xaf
+	b[3] = 0x88
+	_, err = MupfConn.Write(append(tt, b...))
+	assert.Nil(t, err)
+
+	time.Sleep(1 * time.Second)
+
+	// Send ICMP Echo Request through Second RAN
+	gtpHdr, err = hex.DecodeString("32ff00340000000200000000")
+	assert.Nil(t, err)
+
+	// Send through Second RAN
+	ipv4hdr = ipv4.Header{
+		Version:  4,
+		Len:      20,
+		Protocol: 1,
+		Flags:    0,
+		TotalLen: 48,
+		TTL:      64,
+		Src:      net.ParseIP("10.62.0.1").To4(),
+		Dst:      net.ParseIP("10.60.0.102").To4(),
+		ID:       1,
+	}
+	checksum = test.CalculateIpv4HeaderChecksum(&ipv4hdr)
+	ipv4hdr.Checksum = int(checksum)
+
+	v4HdrBuf, err = ipv4hdr.Marshal()
+	assert.Nil(t, err)
+	tt = append(gtpHdr, v4HdrBuf...)
+
+	m = icmp.Message{
+		Type: ipv4.ICMPTypeEcho, Code: 0,
+		Body: &icmp.Echo{
+			ID: 12394, Seq: 1,
+			Data: icmpData,
+		},
+	}
+	b, err = m.Marshal(nil)
+	assert.Nil(t, err)
+	b[2] = 0xaf
+	b[3] = 0x88
+	_, err = SupfConn.Write(append(tt, b...))
+	assert.Nil(t, err)
+
+	time.Sleep(1 * time.Second)
+}
+
 func TestDC(t *testing.T) {
 	// RANs connect to AMF
 	MranConn, SranConn := connectRANsToAMF(t)
@@ -421,4 +506,8 @@ func TestDC(t *testing.T) {
 	// PDU Session Establishment
 	pduSessionEstablishment(t, ue, MranConn)
 	t.Log("PDU Session Establishment successfully")
+
+	// ping test
+	icmpTest(t, MupfConn, SupfConn)
+	t.Log("ICMP write to Master N3 and Second N3 tunnel successfully")
 }
