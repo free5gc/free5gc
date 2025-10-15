3b7aee13c4cd9daec92135bda0b1c0480cea0f23
Alonza0314 <alonza.tu@gmail.com>
2025-10-15 09:42:58 +0000

    feat: xn based handover under dc condition

--- test/nrdc_test.go
+++ test/nrdc_test.go
@@ -35,6 +35,9 @@ const (
 	mranN3Addr = "10.200.200.1"
 	sranN3Addr = "10.200.200.2"
 
+	tMranN3Addr = sranN3Addr
+	tSranN3Addr = mranN3Addr
+
 	googleDNS    = "9.9.10.10"
 	cloudFareDNS = "9.9.9.9"
 
@@ -55,6 +58,11 @@ const (
 	sranULTeid = "00000003"
 	mranDLTeid = "\x00\x00\x00\x01"
 	sranDLTeid = "\x00\x00\x00\x02"
+
+	tMranULTeid = ""
+	tSranULTeid = ""
+	tMranDLTeid = "\x00\x00\x00\x03"
+	tSranDLTeid = "\x00\x00\x00\x04"
 
 	ENABLE_DC_AT_PDU_SESSION_ESTABLISHMENT    = true
 	UN_ENABLE_DC_AT_PDU_SESSION_ESTABLISHMENT = false
@@ -610,6 +618,220 @@ func pduSessionModifyIndication(t *testing.T, ue *test.RanUeContext, MranConn *s
 	time.Sleep(1 * time.Second)
 }
 
+func pathSwitchWithDC(t *testing.T, ue *test.RanUeContext, MranConn *sctp.SCTPConn, SranConn *sctp.SCTPConn) {
+	var n int
+	var sendMsg []byte
+	var recvMsg = make([]byte, 2048)
+	var err error
+
+	buildPathSwitchRequestTransferWithDC := func() ngapType.PathSwitchRequestTransfer {
+		var data ngapType.PathSwitchRequestTransfer
+
+		// DL NG-U UP TNL information
+		upTransportLayerInformation := &data.DLNGUUPTNLInformation
+		upTransportLayerInformation.Present = ngapType.UPTransportLayerInformationPresentGTPTunnel
+		upTransportLayerInformation.GTPTunnel = new(ngapType.GTPTunnel)
+		upTransportLayerInformation.GTPTunnel.GTPTEID.Value = aper.OctetString(tMranDLTeid)
+		upTransportLayerInformation.GTPTunnel.TransportLayerAddress = ngapConvert.IPAddressToNgap(tMranN3Addr, "")
+
+		// DC QoS Flow per TNL Information
+		DCQosFlowPerTNLInformationItem := ngapType.QosFlowPerTNLInformationItem{}
+		DCQosFlowPerTNLInformationItem.QosFlowPerTNLInformation.UPTransportLayerInformation.Present = ngapType.UPTransportLayerInformationPresentGTPTunnel
+
+		// DC Transport Layer Information in QoS Flow per TNL Information
+		DCUpTransportLayerInformation := &DCQosFlowPerTNLInformationItem.QosFlowPerTNLInformation.UPTransportLayerInformation
+		DCUpTransportLayerInformation.Present = ngapType.UPTransportLayerInformationPresentGTPTunnel
+		DCUpTransportLayerInformation.GTPTunnel = new(ngapType.GTPTunnel)
+		DCUpTransportLayerInformation.GTPTunnel.GTPTEID.Value = aper.OctetString(tSranDLTeid)
+		DCUpTransportLayerInformation.GTPTunnel.TransportLayerAddress = ngapConvert.IPAddressToNgap(tSranN3Addr, "")
+
+		// DC Associated QoS Flow List in QoS Flow per TNL Information
+		DCAssociatedQosFlowList := &DCQosFlowPerTNLInformationItem.QosFlowPerTNLInformation.AssociatedQosFlowList
+		DCAssociatedQosFlowItem := ngapType.AssociatedQosFlowItem{}
+		DCAssociatedQosFlowItem.QosFlowIdentifier.Value = 1
+		DCAssociatedQosFlowList.List = append(DCAssociatedQosFlowList.List, DCAssociatedQosFlowItem)
+
+		// Additional DL QoS Flow per TNL Information
+		data.AdditionalDLQosFlowPerTNLInformation = new(ngapType.QosFlowPerTNLInformationList)
+		data.AdditionalDLQosFlowPerTNLInformation.List = append(data.AdditionalDLQosFlowPerTNLInformation.List, DCQosFlowPerTNLInformationItem)
+		
+
+		// Qos Flow Accepted List
+		qosFlowAcceptedList := &data.QosFlowAcceptedList
+		qosFlowAcceptedItem := ngapType.QosFlowAcceptedItem{
+			QosFlowIdentifier: ngapType.QosFlowIdentifier{
+				Value: 1,
+			},
+		}
+		qosFlowAcceptedList.List = append(qosFlowAcceptedList.List, qosFlowAcceptedItem)
+		return data
+	}
+
+	getPathSwitchRequestTransferWithDC := func() []byte {
+		data := buildPathSwitchRequestTransferWithDC()
+		encodeData, err := aper.MarshalWithParams(data, "valueExt")
+		if err != nil {
+			fatal.Fatalf("aper MarshalWithParams error in GetPathSwitchRequestTransferWithDC: %+v", err)
+		}
+		return encodeData
+	}
+
+	buildPathSwitchRequestWithDC := func(pduSessionId int64, amfUeNgapId int64, ranUeNgapId int64) (pdu ngapType.NGAPPDU) {
+		pdu.Present = ngapType.NGAPPDUPresentInitiatingMessage
+		pdu.InitiatingMessage = new(ngapType.InitiatingMessage)
+
+		initiatingMessage := pdu.InitiatingMessage
+		initiatingMessage.ProcedureCode.Value = ngapType.ProcedureCodePathSwitchRequest
+		initiatingMessage.Criticality.Value = ngapType.CriticalityPresentReject
+
+		initiatingMessage.Value.Present = ngapType.InitiatingMessagePresentPathSwitchRequest
+		initiatingMessage.Value.PathSwitchRequest = new(ngapType.PathSwitchRequest)
+
+		pathSwitchRequest := initiatingMessage.Value.PathSwitchRequest
+		pathSwitchRequestIEs := &pathSwitchRequest.ProtocolIEs
+
+		// RAN UE NGAP ID
+		ie := ngapType.PathSwitchRequestIEs{}
+		ie.Id.Value = ngapType.ProtocolIEIDRANUENGAPID
+		ie.Criticality.Value = ngapType.CriticalityPresentReject
+		ie.Value.Present = ngapType.PathSwitchRequestIEsPresentRANUENGAPID
+		ie.Value.RANUENGAPID = new(ngapType.RANUENGAPID)
+
+		rANUENGAPID := ie.Value.RANUENGAPID
+		rANUENGAPID.Value = ranUeNgapId
+
+		pathSwitchRequestIEs.List = append(pathSwitchRequestIEs.List, ie)
+
+		// Source AMF UE NGAP ID
+		ie = ngapType.PathSwitchRequestIEs{}
+		ie.Id.Value = ngapType.ProtocolIEIDSourceAMFUENGAPID
+		ie.Criticality.Value = ngapType.CriticalityPresentReject
+		ie.Value.Present = ngapType.PathSwitchRequestIEsPresentSourceAMFUENGAPID
+		ie.Value.SourceAMFUENGAPID = new(ngapType.AMFUENGAPID)
+
+		aMFUENGAPID := ie.Value.SourceAMFUENGAPID
+		aMFUENGAPID.Value = amfUeNgapId
+
+		pathSwitchRequestIEs.List = append(pathSwitchRequestIEs.List, ie)
+
+		// User Location Information
+		ie = ngapType.PathSwitchRequestIEs{}
+		ie.Id.Value = ngapType.ProtocolIEIDUserLocationInformation
+		ie.Criticality.Value = ngapType.CriticalityPresentIgnore
+		ie.Value.Present = ngapType.PathSwitchRequestIEsPresentUserLocationInformation
+		ie.Value.UserLocationInformation = new(ngapType.UserLocationInformation)
+
+		userLocationInformation := ie.Value.UserLocationInformation
+		userLocationInformation.Present = ngapType.UserLocationInformationPresentUserLocationInformationNR
+		userLocationInformation.UserLocationInformationNR = new(ngapType.UserLocationInformationNR)
+
+		userLocationInformationNR := userLocationInformation.UserLocationInformationNR
+		userLocationInformationNR.NRCGI.PLMNIdentity.Value = aper.OctetString("\x02\xf8\x39")
+		userLocationInformationNR.NRCGI.NRCellIdentity.Value = aper.BitString{
+			Bytes:     []byte{0x00, 0x00, 0x00, 0x00, 0x20},
+			BitLength: 36,
+		}
+
+		userLocationInformationNR.TAI.PLMNIdentity.Value = aper.OctetString("\x02\xf8\x39")
+		userLocationInformationNR.TAI.TAC.Value = aper.OctetString("\x00\x00\x11")
+
+		pathSwitchRequestIEs.List = append(pathSwitchRequestIEs.List, ie)
+
+		// UE Security Capabilities
+		ie = ngapType.PathSwitchRequestIEs{}
+		ie.Id.Value = ngapType.ProtocolIEIDUESecurityCapabilities
+		ie.Criticality.Value = ngapType.CriticalityPresentIgnore
+		ie.Value.Present = ngapType.PathSwitchRequestIEsPresentUESecurityCapabilities
+		ie.Value.UESecurityCapabilities = new(ngapType.UESecurityCapabilities)
+
+		uESecurityCapabilities := ie.Value.UESecurityCapabilities
+		uESecurityCapabilities.NRencryptionAlgorithms.Value = aper.BitString{
+			Bytes:     []byte{0xff, 0xff},
+			BitLength: 16,
+		}
+		uESecurityCapabilities.NRintegrityProtectionAlgorithms.Value = aper.BitString{
+			Bytes:     []byte{0xff, 0xff},
+			BitLength: 16,
+		}
+		uESecurityCapabilities.EUTRAencryptionAlgorithms.Value = aper.BitString{
+			Bytes:     []byte{0xff, 0xff},
+			BitLength: 16,
+		}
+		uESecurityCapabilities.EUTRAintegrityProtectionAlgorithms.Value = aper.BitString{
+			Bytes:     []byte{0xff, 0xff},
+			BitLength: 16,
+		}
+
+		pathSwitchRequestIEs.List = append(pathSwitchRequestIEs.List, ie)
+
+		// PDU Session Resource to be Switched in Downlink List
+		ie = ngapType.PathSwitchRequestIEs{}
+		ie.Id.Value = ngapType.ProtocolIEIDPDUSessionResourceToBeSwitchedDLList
+		ie.Criticality.Value = ngapType.CriticalityPresentReject
+		ie.Value.Present = ngapType.PathSwitchRequestIEsPresentPDUSessionResourceToBeSwitchedDLList
+		ie.Value.PDUSessionResourceToBeSwitchedDLList = new(ngapType.PDUSessionResourceToBeSwitchedDLList)
+
+		pDUSessionResourceToBeSwitchedDLList := ie.Value.PDUSessionResourceToBeSwitchedDLList
+
+		// PDU Session Resource to be Switched in Downlink Item (in PDU Session Resource to be Switched in Downlink List)
+		pDUSessionResourceToBeSwitchedDLItem := ngapType.PDUSessionResourceToBeSwitchedDLItem{}
+		pDUSessionResourceToBeSwitchedDLItem.PDUSessionID.Value = pduSessionId
+		pDUSessionResourceToBeSwitchedDLItem.PathSwitchRequestTransfer = getPathSwitchRequestTransferWithDC()
+
+		pDUSessionResourceToBeSwitchedDLList.List = append(pDUSessionResourceToBeSwitchedDLList.List, pDUSessionResourceToBeSwitchedDLItem)
+
+		pathSwitchRequestIEs.List = append(pathSwitchRequestIEs.List, ie)
+
+		// PDU Session Resource Failed to Setup List
+		// ie = ngapType.PathSwitchRequestIEs{}
+		// ie.Id.Value = ngapType.ProtocolIEIDPDUSessionResourceFailedToSetupListPSReq
+		// ie.Criticality.Value = ngapType.CriticalityPresentIgnore
+		// ie.Value.Present = ngapType.PathSwitchRequestIEsPresentPDUSessionResourceFailedToSetupListPSReq
+		// ie.Value.PDUSessionResourceFailedToSetupListPSReq = new(ngapType.PDUSessionResourceFailedToSetupListPSReq)
+
+		// pDUSessionResourceFailedToSetupListPSReq := ie.Value.PDUSessionResourceFailedToSetupListPSReq
+
+		// // PDU Session Resource Failed to Setup Item (in PDU Session Resource Failed to Setup List)
+		// pDUSessionResourceFailedToSetupItemPSReq := ngapType.PDUSessionResourceFailedToSetupItemPSReq{}
+		// pDUSessionResourceFailedToSetupItemPSReq.PDUSessionID.Value = 11
+		// pDUSessionResourceFailedToSetupItemPSReq.PathSwitchRequestSetupFailedTransfer =
+		// 	getPathSwitchRequestSetupFailedTransferWithDC()
+
+		// pDUSessionResourceFailedToSetupListPSReq.List =
+		// 	append(pDUSessionResourceFailedToSetupListPSReq.List, pDUSessionResourceFailedToSetupItemPSReq)
+
+		// pathSwitchRequestIEs.List = append(pathSwitchRequestIEs.List, ie)
+
+		return pdu
+	}
+
+	getPathSwitchRequestWithDC := func(pduSessionId int64, amfUeNgapId int64, ranUeNgapId int64) ([]byte, error) {
+		message := buildPathSwitchRequestWithDC(pduSessionId, amfUeNgapId, ranUeNgapId)
+		// message.InitiatingMessage.Value.PathSwitchRequest.ProtocolIEs.List = message.InitiatingMessage.Value.PathSwitchRequest.ProtocolIEs.List[0:5]
+		return ngap.Encoder(message)
+	}
+
+	// send path switch request
+	sendMsg, err = getPathSwitchRequestWithDC(10, ue.AmfUeNgapId, ue.RanUeNgapId)
+	if err != nil {
+		t.Fatalf("Failed to get path switch request with DC: %+v", err)
+	}
+	_, err = SranConn.Write(sendMsg) // send from secondary RAN since it is the new master RAN
+	if err != nil {
+		t.Fatalf("Failed to send path switch request to Master RAN: %+v", err)
+	}
+
+	// receive path switch request acknowledge
+	n, err = MranConn.Read(recvMsg)
+	if err != nil {
+		t.Fatalf("Failed to receive path switch request acknowledge from Master RAN: %+v", err)
+	}
+	_, err = ngap.Decoder(recvMsg[:n])
+	if err != nil {
+		t.Fatalf("Failed to decode path switch request acknowledge from Master RAN: %+v", err)
+	}
+}
+
 func TestDC(t *testing.T) {
 	// RANs connect to AMF
 	MranConn, SranConn := connectRANsToAMF(t)
@@ -781,5 +1003,25 @@ func TestDCHandover(t *testing.T) {
 	t.Run("ping test via secondary RAN", func(t *testing.T) {
 		icmpTest(t, SupfConn, sranULTeid, cloudFareDNS, EXPECTED_NO_ERROR)
 		t.Log("ICMP test via secondary RAN successfully")
+	})
+
+	/*
+		Start handover:
+			- Master RAN will handover to secondary RAN
+			- Means that secondary RAN will be the new master RAN, and master RAN will be the new secondary RAN
+	*/
+	pathSwitchWithDC(t, ue, MranConn, SranConn)
+	t.Log("Path Switch with DC successfully")
+
+	// ping test via new master RAN (original secondary RAN)
+	t.Run("ping test via new master RAN (original secondary RAN)", func(t *testing.T) {
+		icmpTest(t, SupfConn, tMranULTeid, googleDNS, EXPECTED_NO_ERROR)
+		t.Log("ICMP test via new master RAN successfully")
+	})
+
+	// ping test via new secondary RAN (original master RAN)
+	t.Run("ping test via new secondary RAN (original master RAN)", func(t *testing.T) {
+		icmpTest(t, MupfConn, tSranULTeid, cloudFareDNS, EXPECTED_NO_ERROR)
+		t.Log("ICMP test via new secondary RAN successfully")
 	})
 }
