dfcf8f69c2a7f04396ebd73dcb4e91cf54c1516a
c9274326 <c9274326@gmail.com>
2025-12-17 10:26:56 +0000

    fix: milenage logic in test directory

--- test/milenage.go
+++ test/milenage.go
@@ -1,69 +1,34 @@
 package test
 
 import (
-	"encoding/hex"
-
 	"github.com/free5gc/util/milenage"
 )
 
 func MilenageF1(opc, k, rand, sqn, amf []byte, macA, macS []byte) error {
-	ik, ck, xres, autn, err := milenage.GenerateAKAParameters(opc, k, rand, sqn, amf)
+
+	mac_a, mac_s, err := milenage.F1(opc, k, rand, sqn, amf)
 	if err != nil {
 		return err
 	}
-	// Suppress unused variable warnings
-	_ = ik
-	_ = ck
-	_ = xres
 
-	// AUTN = (SQN xor AK) || AMF || MAC-A
-	// MAC-A is the last 8 bytes of AUTN
-	if len(autn) >= 8 && macA != nil {
-		copy(macA, autn[len(autn)-8:])
+	if macA != nil {
+		copy(macA, mac_a)
 	}
 
-	// For MAC-S, use resync AMF (0000)
 	if macS != nil {
-		resyncAMFBytes, err := hex.DecodeString("0000")
-		if err != nil {
-			return err
-		}
-		ikS, ckS, xresS, autnS, err := milenage.GenerateAKAParameters(opc, k, rand, sqn, resyncAMFBytes)
-		if err != nil {
-			return err
-		}
-		// Suppress unused variable warnings
-		_ = ikS
-		_ = ckS
-		_ = xresS
-
-		if len(autnS) >= 8 {
-			copy(macS, autnS[len(autnS)-8:])
-		}
+		copy(macS, mac_s)
 	}
 
 	return nil
 }
 
 func MilenageF2345(opc, k, rand []byte, res, ck, ik, ak, akstar []byte) error {
-	// Use GenerateAKAParameters to get basic parameters
-	ikOut, ckOut, resOut, autn, err := milenage.GenerateAKAParameters(opc, k, rand, make([]byte, 6), make([]byte, 2))
+
+	resOut, ckOut, ikOut, akOut, akstarOut, err := milenage.F2345(opc, k, rand)
 	if err != nil {
 		return err
 	}
 
-	// Use GenerateKeysWithAUTN to get AK
-	sqnhe, akOut, ikOut2, ckOut2, resOut2, err := milenage.GenerateKeysWithAUTN(opc, k, rand, autn)
-	if err != nil {
-		return err
-	}
-	// Suppress unused variable warnings
-	_ = sqnhe
-	_ = ikOut2
-	_ = ckOut2
-	_ = resOut2
-
-	// Copy results to output parameters
 	if res != nil {
 		copy(res, resOut)
 	}
@@ -77,9 +42,9 @@ func MilenageF2345(opc, k, rand []byte, res, ck, ik, ak, akstar []byte) error {
 		copy(ak, akOut)
 	}
 	if akstar != nil {
-		// For AK*, we need to use a different SQN, but due to API limitations, we use the same value for now
-		copy(akstar, akOut)
+		copy(akstar, akstarOut)
 	}
 
 	return nil
 }
+
