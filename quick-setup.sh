c10c8cb6812c3a52a4130258e1ece830c566ab21
Alonza0314 <alonza.tu@gmail.com>
2026-02-20 19:19:44 +0000

    chore: add tab

--- quick-setup.sh
+++ quick-setup.sh
@@ -216,9 +216,9 @@ install_yarn() {
 
 print_counts() {
     log_info "Task Summary:"
-    log_info "Success: $SUCCESS_COUNT"
-    log_info "Skip: $SKIP_COUNT"
-    log_info "Fail: $FAIL_COUNT"
+    log_info "  Success: $SUCCESS_COUNT"
+    log_info "  Skip: $SKIP_COUNT"
+    log_info "  Fail: $FAIL_COUNT"
 }
 
 main() {
